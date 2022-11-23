3fb32918fab53709dac28ade304788a4eaeea0cc
EC2 Default User <ec2-user@ip-172-31-53-53.ec2.internal>
2022-11-23 15:55:07 +0000

    rebuild docker

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 # Containerized FastAPI
 
 `docker build .`
-`docker run -p 127.0.0.1:8080:8080 ce95e8e0462a`
+`docker run -p 127.0.0.1:8080:8080 378c80e24481`
+
+# deployed domain 
+`https://6wwcyqaxeq.us-east-1.awsapprunner.com `
--- newslib/newsgen.py
+++ newslib/newsgen.py
@@ -10,22 +10,21 @@ def newsgen(newselement):
     soup = BeautifulSoup(r.text, "html.parser")
 
     news_urls = []
-    for link in soup.find_all('a'):
-        link_url = link.get('href')
-        #Discard "None"
+    for link in soup.find_all("a"):
+        link_url = link.get("href")
+        # Discard "None"
         if link_url:
-            if "magazine/archive/2022" in link_url:
+            if "/most-popular" in link_url:
                 news_urls.append(link_url)
 
-    one_url = news_urls[0]
-    res_one_url = requests.get(one_url)
-    soup_one_url = BeautifulSoup(res_one_url.content, 'html.parser')
-
-    lines = soup_one_url.text
-    res2 = lines.split(",")
-    key_line = []
-    for line in res2:
-        if newselement in line:
-            key_line.append(line)
+    for url in news_urls:
+        res_one_url = requests.get(url)
+        soup_one_url = BeautifulSoup(res_one_url.content, "html.parser")
+        lines = soup_one_url.text
+        res2 = lines.split(",")
+        key_line = []
+        for line in res2:
+            if newselement in line:
+                key_line.append(line)
 
     return key_line
